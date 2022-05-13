ddde9d229d92907a1c2696cab8422a87bbb634da
Rob Davis <robd@youi.com>
2022-05-13 16:29:20 +1000

    Added background service

--- src/StravaUploader/Program.cs
+++ src/StravaUploader/Program.cs
@@ -1,7 +1,24 @@
-﻿// Service - https://docs.microsoft.com/en-us/dotnet/core/extensions/windows-service
-
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using StravaUploader;
 
 IConfiguration config = new ConfigurationBuilder()
     .AddJsonFile("config.json")
     .Build();
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .UseWindowsService(options =>
+    {
+        options.ServiceName = "Strava Upload Service";
+    })
+    .ConfigureServices(services =>
+    {
+        services.Configure<Config>(config);
+        services.AddScoped<IAuthListener, AuthListener>();
+        services.AddSingleton<DeviceListener>();
+        services.AddHostedService<StravaBackgroundService>();
+    })
+    .Build();
+
+await host.RunAsync();
---
+++ src/StravaUploader/StravaBackgroundService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace StravaUploader
+{
+    internal class StravaBackgroundService : BackgroundService
+    {
+        private readonly DeviceListener _listener;
+        internal StravaBackgroundService(DeviceListener deviceListener)
+        {
+            _listener = deviceListener;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            try
+            {
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    _listener.DeviceFound += DeviceFound;
+                }
+            }
+            catch (Exception ex)
+            {
+                // TODO: Log exception
+                Environment.Exit(1);
+            }
+        }
+
+        private void DeviceFound(object? sender, DeviceFoundArgs e)
+        {
+            // TODO: Make this work
+            Console.WriteLine(e.Path);
+        }
+    }
+}
